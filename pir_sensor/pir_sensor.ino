9abfbf3f2197a312b2123b38a0d23cb053bf7612
Alfred Kondoro <alfredkondoro97@gmail.com>
2020-11-01 02:32:05 +0300

    W.15.1 - Ultrasonic PIR Sensor

--- pir_sensor/pir_sensor.ino
+++ pir_sensor/pir_sensor.ino
@@ -1,13 +1,33 @@
+int trigPin = 7;
+int echoPin = 8;
+
 void setup() {
-  pinMode(8, INPUT);
-  pinMode(9, OUTPUT);
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
-  if(digitalRead(8) == HIGH){
-    digitalWrite(9, HIGH);
-  }
-  else{
-    digitalWrite(9, LOW);
-  }
+  long duration, inches, cm;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(100);
 }
+
+long microsecondsToInches (long microseconds){
+  return microseconds / 74 / 2;
+  }
+long microsecondsToCentimeters (long microseconds){
+  return microseconds / 29 / 2;
+  }
